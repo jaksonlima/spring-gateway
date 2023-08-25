2e05ec3403c1088a37ca83b1f3d47d73be1af761
jaksonlima <jaksonsneider@gmail.com>
2023-08-25 14:30:40 -0300

    first commit

---
+++ src/main/java/com/app/gateway/gateway/PersonGateway.java
@@ -0,0 +1,20 @@
+package com.app.gateway.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PersonGateway {
+
+    @Bean
+    public RouteLocator personRouteLocator(final RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_route", r -> r.path("/api/**")
+                        .filters(f -> f.setPath("/"))
+                        .uri("http://httpbin.org"))
+                .build();
+    }
+
+}
